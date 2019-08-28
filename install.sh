060f1291d46d948f2b48f9227c0534fc165415dc
Erwan Queffélec <airone.k@gmail.com>
2019-08-28 04:07:25 +0200

    Improve and fix install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ DONE="${BOLD}${COLOR_GREEN}✓${COLOR_RESET}"
 FAIL="${BOLD}${COLOR_RED}✗${COLOR_RESET}"
 WORK="${BOLD}${COLOR_BLUE}⥁${COLOR_RESET}"
 
-MY_VIM_INSTALL_DIR=${MY_ZSH_INSTALL_DIR:-"${HOME}/Code/tools/"}
-MY_VIM="${MY_VIM_INSTALL_DIR}/vim-config"
+MY_TOOLS_DIR=${MY_TOOLS_DIR:-"${HOME}/Code/tools"}
+MY_VIM="${MY_TOOLS_DIR}/vim-config"
 
 echo "${BOLD}Welcome back, Erwan !${COLOR_RESET}"
 echo "${BOLD}Have you been a good boy and installed the correct dependencies ;) ?${COLOR_RESET}"
@@ -25,11 +25,16 @@ else
   echo "${DONE} vim is installed! Maybe there's hope for you after all!"
 fi
 
-if [ ! -d "${MY_ZSH_INSTALL_DIR}" ]
+if [ ! -d "${MY_TOOLS_DIR}" ]
 then
-  echo "${WORK} Creating the installation directory"
-  mkdir -p ${MY_ZSH_INSTALL_DIR}
-  git clone https://github.com/3rwww1/vim-config
+  echo "${WORK} Creating the tools directory"
+  mkdir -p ${MY_TOOLS_DIR}
+
+if [ ! -d "${MY_VIM}" ]
+then
+  pushd ${MY_TOOLS_DIR}
+    git clone https://github.com/3rwww1/vim-config
+  popd
 else
   echo "${WORK} Pulling latest configuration..."
   pushd ${MY_VIM}
@@ -38,7 +43,7 @@ else
   popd
 fi
 
-pushd ${MY_VIM}/zsh-config
+pushd ${MY_VIM}/vim-config
   mkdir -p ${HOME}/.vim
   rm ${HOME}/.vim/vimrc
   ln -sf vimrc ${HOME}/vim/.vimrc
